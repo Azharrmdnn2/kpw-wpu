e4c72773b84a5134ba379cf7f1a189a7e56fe149
Azharrmdnn2 <azhar118428@gmail.com>
2022-10-02 02:45:44 +0700

    insert data

--- pertemuan10/functions.php
+++ pertemuan10/functions.php
@@ -22,3 +22,24 @@ function query($query)
   }
   return $rows;
 }
+
+
+function tambah($data)
+{
+  $conn = koneksi();
+
+  $nama = htmlspecialchars($data['nama']);
+  $nrp = htmlspecialchars($data['nrp']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO mahasiswa VALUE ('', '$nama', '$nrp', '$email', '$jurusan', '$gambar')";
+  $result = mysqli_query($conn, $query);
+
+
+  // untuk mengetahui error
+  echo mysqli_error($conn);
+  // ngasitau ada baris yang berubah atau data yang bertambah
+  return mysqli_affected_rows($conn);
+}
